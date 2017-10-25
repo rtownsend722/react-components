6da5737398c5da1cec884981db5b54c83bf39238
Rebecca Townsend <rebecca.townsend722@gmail.com>
2017-10-25 12:07:38 -0700

    Build react components

--- app.js
+++ app.js
@@ -1,1 +1,42 @@
-// TODO
+
+var GroceryList = (props) => (
+  <div>
+    <h2>My Grocery List</h2>
+      <ul>
+        {props.items.map(item =>        
+          <GroceryListItem item={item} />
+        )}
+      </ul>
+  </div>
+);
+
+
+class GroceryListItem extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      purchased: false
+    };
+  }
+
+  onListItemHover() {
+    this.setState({
+      purchased: !this.state.purchased
+    });
+  }
+
+  render() {
+
+    var style = {
+      fontWeight: this.state.purchased ? 'bold' : 'normal'
+    };
+
+    return (
+      <li style={style} onMouseEnter={this.onListItemHover.bind(this)} onMouseLeave={this.onListItemHover.bind(this)}>{this.props.item}</li>
+    );
+  }
+
+}
+
+ReactDOM.render(<GroceryList items={['cheese', 'bread', 'soup', 'potatoes']}/>, document.getElementById("app"));
